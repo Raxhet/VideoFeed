107f3858d7da7bce2a90f0ca6ab4e6465fdd94a6
Merkulenko Ilia <imerkulenko@Ilas-MacBook-Pro.local>
2023-07-10 21:53:20 +0800

    Start project

---
+++ VideoFeed/VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedApp.swift
+//  VideoFeed
+//
+//  Created by Илья Меркуленко on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
